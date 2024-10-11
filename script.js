64cec0ff014499014433dcf318d4e42f6eba24f0
martnx <martinmart2002@gmail.com>
2024-10-11 13:10:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//Addition function
+function add(a, b){
+
+}
+
+//Substract function
+function subtract(a, b){
+    
+}
+
+//Multiply function
+function multiply(a, b){
+    
+}
+
+//Divide function
+function divide(a, b){
+    
+}
