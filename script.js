1af2c9ba2c611428227a908b36ef12046325b1e9
martnx <martinmart2002@gmail.com>
2024-10-14 22:52:36 +0800

    added function

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 
 //Addition function
 function add(a, b){
-
+    let x = a + b;
+    return x;
 }
 
 //Substract function
 function subtract(a, b){
-    
+    let x = a - b;
+    return x;
 }
 
 //Multiply function
 function multiply(a, b){
-    
+    let x = a * b;
+    return x;
 }
 
 //Divide function
 function divide(a, b){
-    
+    let x = a / b;
+    return x;
 }
+
+function operate(a, b){
+    return add(a, b);
+}
+
+console.log(operate(7.5, 7.5));
