6f679101d229d33ad10456d29aae418ca78ed31c
Jonh Martin Legaspi <martinmart2002@gmail.com>
2024-11-04 01:16:16 +0800

    commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
 const resultDiv = document.querySelector('.result-div');
 
+//true = first input || false = second number
+let switchNumber = true;
+//function for first number
+function firstNumber(number){
+
+}
+
+
+x``
 //Addition function
 function add(a, b){
     let x = a + b;
@@ -25,8 +33,26 @@ function divide(a, b){
     return x;
 }
 
-function operate(a, b){
-    return add(a, b);
+function operate(operate){
+    let container = 0;
+    switch(operate){
+        case "add":
+            container = add(firstNum, secondNum);
+            break;
+        case "subtract":
+            container = subtract(firstNum, secondNum);
+            break;
+        case "multiply":
+            container = multiply(firstNum, secondNum);
+            break;
+        case "divide":
+            container = divide(firstNum, secondNum);
+            break;
+        default:
+            console.log("error! not yet defined!");
+
+    }
+    return container;
 }
 
 
@@ -38,20 +64,29 @@ for(let i = 0; i < allBtn.length; i++){
         const target = e.target
         switch(target.id){
             //expression and clear delete   
-            case "button-add": console.log("this is add button"); break;
-
-            case "button-substract": 
+            case "button-add":
+                operate("add");
+                console.log("this is add button"); 
+                break;
+            case "button-subtract": 
+                operate("subtract");
                 console.log("this is substract button"); 
                 break;
-            case "button-multiply": 
+            case "button-multiply":
+                operate("multiply");
                 console.log("this is multiply button"); 
                 break;
             case "button-divide": 
+                operate("divide");
                 console.log("this is divide button"); 
+                break;
+            case "button-equal":
+                console.log(operate());
                 break;
             //number button
             case "button-one": //button 1
-                add = 1; 
+                if(switchNumber == true) firstNumber("1");
+                else secondNum
                 break;
             case "button-two": //button 2
                 add = 2; 
@@ -78,7 +113,7 @@ for(let i = 0; i < allBtn.length; i++){
                 add = 9; 
                 break;
             case "button-zero": //button 0
-                add = ; 
+                add = 0; 
                 break;
             
             default:
@@ -87,4 +122,3 @@ for(let i = 0; i < allBtn.length; i++){
     });
 }
 
-console.log(add);
